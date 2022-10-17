b6102ca83370d037e67cb496636a06aba355dfc0
Ahmet <ahmetbeler1739@gmail.com>
2022-10-17 16:58:34 +0300

    app

--- src/App.js
+++ src/App.js
@@ -1,39 +1,30 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/index";
 import Home from "./pages/home/index";
-import Item from "./pages/Item/index"
-import {useDispatch} from "react-redux"
-import { fetchTopSellers} from "./redux/slices/topSellerSlice";
+import Item from "./pages/hotbindItem/index"
+import { useDispatch } from "react-redux"
+import { fetchTopSellers } from "./redux/slices/topSellerSlice";
 import { fetchHotBids } from "./redux/slices/hotBidsSlice";
 import './App.css';
 import { useEffect } from "react";
 
 function App() {
-  const dispatch =useDispatch()
+  const dispatch = useDispatch()
 
   useEffect(() => {
     dispatch(fetchTopSellers())
     dispatch(fetchHotBids())
   }, [])
-  
+
   return (
     <div className="App">
 
       <Router>
         <Routes>
-
           <Route path="/" exact element={<Layout><Home /></Layout>} />
-          <Route path="/ıtem" element={<Layout><Item /></Layout>} />
+          <Route path="/item/:id" element={<Layout><Item /></Layout>} />
         </Routes>
-
-
-
       </Router>
-
-
-
-
-
 
     </div>
   );
