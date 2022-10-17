6cd52cbae71841e2e24754e115966ed19376eded
Ahmet <ahmetbeler1739@gmail.com>
2022-10-17 16:59:03 +0300

    index.js

--- src/components/hotbind/index.js
+++ src/components/hotbind/index.js
@@ -1,59 +1,59 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import "./index.css"
 import HotbindCard from "../hotbind-card/index"
-import {useSelector} from "react-redux"
+import { useSelector } from "react-redux"
 
 
 
 export default function Hotbind() {
- const {hotBids} = useSelector(state =>state.hotBids) 
-const [count,setCount]=useState(8)
+    const { hotBids } = useSelector(state => state.hotBids)
+    const [count, setCount] = useState(8)
 
-const handleSetCount = () =>{
-    if (count <= hotBids?.length){
-        setCount ( count + 8 )
+    const handleSetCount = () => {
+        if (count <= hotBids?.length) {
+            setCount(count + 8)
 
-    } else{
-        setCount(8)
+        } else {
+            setCount(8)
+        }
+
     }
-    
-}
 
 
- console.log(hotBids) 
+    /*  console.log(hotBids)  */
 
 
     return (
         <>
-         <p className="hot-title">Hotbind</p>
-        <div className="hotbind ">
-           
-           
-            {
+            <p className="hot-title">Hotbind</p>
+            <div className="hotbind ">
 
-                hotBids?.slice(0,count)?.map(item =>{
-                    return ( <HotbindCard key={item.id} item={item}/>)
-                })
 
-               
- 
-            }
-            
-          
+                {
 
-                
+                    hotBids?.slice(0, count)?.map(item => {
+                        return (<HotbindCard key={item?.id} item={item} />)
+                    })
 
-        </div>
-     <div onClick={handleSetCount}>
-     {
-            count <= hotBids?.length ? 
-            <img src="./img/load.svg" alt="load" className="load" ></img>
-            :
-            <div className="showles" ><span className="show">Show Less</span></div>
-        }
-     </div>
-        
-     <hr className="hr"></hr>
+
+
+                }
+
+
+
+
+
+            </div>
+            <div onClick={handleSetCount}>
+                {
+                    count <= hotBids?.length ?
+                        <img src="./img/load.svg" alt="load" className="load" ></img>
+                        :
+                        <div className="showles" ><span className="show">Show Less</span></div>
+                }
+            </div>
+
+            <hr className="hr"></hr>
         </>
     )
 }
